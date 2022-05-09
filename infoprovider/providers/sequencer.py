a9cfc58ba2cc60b6914fab602780775ac2c6b4ef
Gennaro Tortone <gennaro.tortone@na.infn.it>
2022-05-09 18:06:03 +0200

    added rundescription for sequencer infoprovider

--- infoprovider/providers/sequencer.py
+++ infoprovider/providers/sequencer.py
@@ -6,6 +6,9 @@ class SequencerProvider:
 
         self.data['loopCounter'] = odb['State']['Loop counter'][0]
         self.data['loopLimit'] = odb['State']['Loop n'][0]
+        for line in odb['Script']['Lines']:
+            if line.startswith('RUNDESCRIPTION'):
+                self.data['runDescription'] = line.split('"')[1]
 
     def getData(self):
         return self.data
