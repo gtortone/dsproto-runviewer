0884fa47f26fb5bf49e8a58d5ad138df37fdd585
Gennaro Tortone <gtortone@gmail.com>
2022-07-13 10:08:15 +0200

    skip hv slot if not defined

--- infoprovider/providers/hv.py
+++ infoprovider/providers/hv.py
@@ -12,32 +12,33 @@ class HVProvider:
         slotIndex = 0
         for slot in self.slots:
             moduleItem = {}
-            moduleItem['name'] = odb['Status'][f'Slot {slot}']['Model']
-            moduleItem['description'] = odb['Status'][f'Slot {slot}']['Description']
+            if slot in odb['Status']:
+                moduleItem['name'] = odb['Status'][f'Slot {slot}']['Model']
+                moduleItem['description'] = odb['Status'][f'Slot {slot}']['Description']
 
-            channelList = []
-            for channel in channels[slotIndex]:
-                channelItem = {}
-                channelItem['number'] = channel
-                channelItem['name'] = odb['Settings'][f'Slot {slot}']['ChName'][channel]
-                metricList = []
-                for metric in metrics[slotIndex]:
-                    metricItem = {}
-                    metricItem['name'] = metric
-                    subdir = 'Settings' if 'Set' in metric else 'Status'
-                    metricItem['value'] = odb[subdir][f'Slot {slot}'][metric][channel]
-                    if metric.find("(") != -1:
-                        metricItem['unit'] = metric[metric.find("(")+1:metric.find(")")]
+                channelList = []
+                for channel in channels[slotIndex]:
+                    channelItem = {}
+                    channelItem['number'] = channel
+                    channelItem['name'] = odb['Settings'][f'Slot {slot}']['ChName'][channel]
+                    metricList = []
+                    for metric in metrics[slotIndex]:
+                        metricItem = {}
+                        metricItem['name'] = metric
+                        subdir = 'Settings' if 'Set' in metric else 'Status'
+                        metricItem['value'] = odb[subdir][f'Slot {slot}'][metric][channel]
+                        if metric.find("(") != -1:
+                            metricItem['unit'] = metric[metric.find("(")+1:metric.find(")")]
 
-                    metricList.append(metricItem)
+                        metricList.append(metricItem)
 
-                channelItem['metrics'] = metricList
+                    channelItem['metrics'] = metricList
 
-                channelList.append(channelItem)
+                    channelList.append(channelItem)
 
-            moduleItem['channels'] = channelList
-            moduleList.append(moduleItem)
-            slotIndex = slotIndex + 1
+                moduleItem['channels'] = channelList
+                moduleList.append(moduleItem)
+                slotIndex = slotIndex + 1
 
         self.data['modules'] = moduleList
 
