9593bc5c6f7c4f00fc921efc0204463c539d0f49
Gennaro Tortone <gtortone@gmail.com>
2024-11-12 21:48:54 +0100

    force ODB value as string in shift infoprovider

--- infoprovider/providers/shift.py
+++ infoprovider/providers/shift.py
@@ -9,7 +9,7 @@ class ShiftProvider:
         for k,v in odb.items():
             if "key" in k:
                 continue
-            self.data[camelCase(k)] = v.replace("\"", "\\\"")
+            self.data[camelCase(k)] = str(v).replace("\"", "\\\"")
 
     def getData(self):
         return self.data
